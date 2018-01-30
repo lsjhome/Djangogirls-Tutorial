c9fca56a35c0c84e9b367f988404b8adcc1d02e1
lsjhome <lsjhome007@gmail.com>
2018-01-30 15:59:03 +0900

    post_detail에 동적으로 pk값을 받을 수 있도록 처리

--- django/blog/urls.py
+++ django/blog/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path
+import re
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.post_list),
-    path('detail/', views.post_detail),
+    # path('', views.post_detail),
+
+    # 3/
+    # 53/
+    # 53/asdf/ <-
+    re_path(r'(?P<pk>\d+)/$', views.post_detail)
+    # 숫자가 1개 이상 반복되는 경우를 정규표현식으로 구현하되
+    # 해당 반복구간을 그룹으로 묶고, 그룹 이름을 'pk'로 지정
+    # re.complie(r'(?P<pk>\d+)')
 ]
